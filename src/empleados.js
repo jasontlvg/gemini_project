b81f228f676c4e15afbd171ef0455fd47f57e51b
shieldvg <shieldvg@gmail.com>
2019-06-30 22:49:35 -0700

    Version con editar usuarios lista

--- src/empleados.js
+++ src/empleados.js
@@ -1,1 +1,18 @@
 const x= require('./scss/empleados.scss');
+
+let a= document.getElementById('lala');
+let g= document.getElementById('girlIcon');
+let b= document.getElementById('boyIcon');
+
+// alert('as;ldksal;k');
+a.addEventListener('click', function(e){
+    if(e.target.value == 'Masculino'){
+        b.classList.remove('disabled');
+        g.classList.remove('active');
+        console.log('Cambiando a Masculino');
+    }else if(e.target.value == 'Femenino'){
+        g.classList.add('active');
+        b.classList.add('disabled');
+        console.log('Cambiando a Femenino');
+    }
+});
